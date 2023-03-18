29cd9cf7d918e3aee53ea599326fd50c251ca3bb
Kexin Xie <kexin.xie@salesforce.com>
2023-03-17 17:14:30 -0700

    sightly better CLI desc (#6)

--- cmd/service.go
+++ cmd/service.go
@@ -12,13 +12,8 @@ import (
 // serviceCmd represents the service command
 var serviceCmd = &cobra.Command{
 	Use:   "service",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Avalaible services to run",
+	Long:  `All the available services available as sub commands`,
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
@@ -26,14 +21,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(serviceCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// serviceCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// serviceCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
