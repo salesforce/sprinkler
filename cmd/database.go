05c5e6d37881d3db47c9a339d2352c3ecddd3b41
Kexin Xie <kexin.xie@salesforce.com>
2023-03-17 17:25:59 -0700

    better database defaults with CLI flags too (#8)

--- cmd/database.go
+++ cmd/database.go
@@ -9,12 +9,34 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"mce.salesforce.com/sprinkler/database"
 )
 
 var dryRunOpt = false
+
+type DatabaseCmdOpt struct {
+	Host     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+const (
+	DBFlagHost       string = "host"
+	DBConfigHost            = "db.host"
+	DBFlagUser              = "user"
+	DBConfigUser            = "db.user"
+	DBFlagPassword          = "password"
+	DBConfigPassword        = "db.password"
+	DBFlagDBName            = "dbname"
+	DBConfigDBName          = "db.dbname"
+	DBFlagSSLMode           = "sslmode"
+	DBConfigSSLMode         = "db.sslmode"
+)
 
 // databaseCmd represents the database command
 var databaseCmd = &cobra.Command{
@@ -40,6 +62,40 @@ func init() {
 	rootCmd.AddCommand(databaseCmd)
 
 	// make this persistent flag available to all sub commands
-	databaseCmd.PersistentFlags().BoolVar(&dryRunOpt, "dry-run", false, "Dry Run")
+	databaseCmd.PersistentFlags().BoolVar(
+		&dryRunOpt,
+		"dry-run",
+		false,
+		"Dry Run",
+	)
 
+	databaseCmd.PersistentFlags().String(DBFlagHost, "db", "database host")
+	viper.BindPFlag(
+		DBConfigHost,
+		databaseCmd.PersistentFlags().Lookup(DBFlagHost),
+	)
+
+	databaseCmd.PersistentFlags().String(DBFlagUser, "postgres", "database username")
+	viper.BindPFlag(
+		DBConfigUser,
+		databaseCmd.PersistentFlags().Lookup(DBFlagUser),
+	)
+
+	databaseCmd.PersistentFlags().String(DBFlagPassword, "sprinkler", "database password")
+	viper.BindPFlag(
+		DBConfigPassword,
+		databaseCmd.PersistentFlags().Lookup(DBFlagPassword),
+	)
+
+	databaseCmd.PersistentFlags().String(DBFlagDBName, "postgres", "database name")
+	viper.BindPFlag(
+		DBConfigDBName,
+		databaseCmd.PersistentFlags().Lookup(DBFlagDBName),
+	)
+
+	databaseCmd.PersistentFlags().String(DBFlagSSLMode, "disable", "database name")
+	viper.BindPFlag(
+		DBConfigSSLMode,
+		databaseCmd.PersistentFlags().Lookup(DBFlagSSLMode),
+	)
 }
